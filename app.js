14e38a5a7ddb4d3e0a3ac95ec788d4dfce83300f
douglaspiero <douglaspiero@gmail.com>
2022-02-03 21:56:52 -0300

    request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const requestIp = require('request-ip');
+let request = require('request');
 const port = 3000;
 
 
@@ -9,6 +10,30 @@ app.get('/', (req,res) => {
     console.log(clientIp);
     res.json({clientIp});
 });
+
+
+app.get('/ip', (req,res) => {
+    let url = `http://ip-api.com/json`
+    let dados = '';
+ 
+    request(url, function (err, response, body) {
+         if(err){
+         console.log('error:', err);
+   } else {
+       let ipInfo = JSON.parse(body);
+       dados = `IP: ${ipInfo.query}
+                Country: ${ipInfo.country}
+                City: ${ipInfo.city}
+                Region: ${ipInfo.regionName}
+                Lat: ${ipInfo.lat}
+                Lon: ${ipInfo.lon}
+                Organization: ${ipInfo.org}
+                `
+       res.json({ipInfo});
+        }
+    });
+});
+
 
 
 app.listen(process.env.PORT || port, () => {
