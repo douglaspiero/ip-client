f5e795ccbf98a522b5d27dcc787bb58fff5a2310
douglaspiero <douglaspiero@gmail.com>
2022-02-03 22:27:25 -0300

    ip remore

--- app.js
+++ app.js
@@ -8,30 +8,24 @@ const port = 3000;
 app.get('/', (req,res) => {
     var clientIp = requestIp.getClientIp(req); 
     console.log(clientIp);
-    res.json({clientIp});
+
+    let url = `http://ip-api.com/json/${clientIp}`;
+    let dados = "";
+
+    request(url, function (err, response, body) {
+        if(err){
+            console.log('error:', err);
+        } else {
+            let ip = JSON.parse(body);
+            console.log(ip)
+           res.json(ip);
+        }
+     });
 });
 
 
 app.get('/ip', (req,res) => {
-    let url = `http://ip-api.com/json`
-    let dados = '';
  
-    request(url, function (err, response, body) {
-         if(err){
-         console.log('error:', err);
-   } else {
-       let ipInfo = JSON.parse(body);
-       dados = `IP: ${ipInfo.query}
-                Country: ${ipInfo.country}
-                City: ${ipInfo.city}
-                Region: ${ipInfo.regionName}
-                Lat: ${ipInfo.lat}
-                Lon: ${ipInfo.lon}
-                Organization: ${ipInfo.org}
-                `
-       res.json({ipInfo});
-        }
-    });
 });
 
 
